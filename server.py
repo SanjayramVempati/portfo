3a11702d40efda4814f40630ac8ee508d84ecaae
SanjayramVempati <101228879+SanjayramVempati@users.noreply.github.com>
2022-03-17 15:54:41 -0400

    Edit One: Removed unnecacary code

--- server.py
+++ server.py
@@ -1,39 +1,15 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request
 import csv
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def my_home():
+def home():
     return render_template('index.html')
 
-@app.route('/<string:page_name>')
-def html_page(page_name):
-    return render_template(page_name)
+@app.route('/<string:page>')
+def pagers(page):
+    return render_template(page)
 
 
-
-def write_to_csv(data):
-  with open('database.csv', newline='', mode='a') as database2:
-    email = data["email"]
-    subject = data["subject"]
-    message = data["message"]
-    
-    csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    csv_writer.writerow([email,subject,message])
-    csv_writer.writerow([data])
-
-
-@app.route('/submit_form', methods=['POST', 'GET'])
-def submit_form():
-    if request.method == 'POST':
-      try:
-        data = request.form.to_dict()
-        print(data)
-        write_to_csv()
-        return redirect('./thankyou.html')
-      except:
-        return 'did not save to database'
-    else:
-      return 'something went wrong. Try again!'
